6a00c6233a2084170b581d1176287863e3224794
DonVicii <donvicii09@gmail.com>
2018-07-23 00:08:14 +0800

    simple study

---
+++ src/main/java/servlet/InitialID.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.UUID;
+
+public class InitialID implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        servletContext.setAttribute("uuid", uuid);
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
